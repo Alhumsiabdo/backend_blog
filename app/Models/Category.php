cbaa50d8824ef33d89b247159039dbf67193b5f8
abdullah <abdullah.alhomsi@cems-it.com>
2024-07-11 18:01:58 +0300

    make relation models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Category extends Model
 {
     use HasFactory;
+
+    protected $table = "categories";
+
+    protected $fillable = [
+        'name',
+        'slug',
+    ];
+
+    public function post() : BelongsToMany
+    {
+        return $this->belongsToMany(Post::class, 'post_category');
+    }
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
     use HasFactory;
+
+    protected $table = "tags";
+
+    protected $fillable = [
+        'name',
+        'slug'
+    ];
+
+    public function post() : BelongsToMany
+    {
+        return $this->belongsToMany(Post::class, 'post_tag');
+    }
 }
